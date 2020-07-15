20e09fce632b3a2217c5bcebe179a720dc501f65
Nik McKnight <60225340+NikMcKnight@users.noreply.github.com>
2020-07-15 18:01:51 -0500

    Got encode/decode working and hopefully put the files in the correct place this time

--- Project2/Project2/Source.cpp
+++ Project2/Project2/Source.cpp
@@ -7,9 +7,14 @@
 #include "Morse_Tree.h"
 #include <algorithm>
 #include <sstream>
+#include "Functions.h"
 
 using namespace std;
 
+<<<<<<< HEAD
+
+
+=======
 int get_val(string code) {
 	string result_str = "";
 	int result = 0;
@@ -22,26 +27,26 @@ int get_val(string code) {
 	res_stream >> result;
 	return result;
 }
+>>>>>>> ea741d83787f70d0189ebed67428504d70a814d5
 
 int main() {
 
-	ifstream input_file("morse.txt");
-	vector<Morse_Letter> morse_vec;
-	//vector<string> string_vec;
-	string temp, temp_char, temp_code;
-	//int temp_int;
-	while (getline((input_file), temp)) {
+	map<string, string> letters_map_enc, letters_map_dec; // maps for encoding and decoding
+	vector<Morse_Letter> morse_vec;  
+	read_file_to_vec(morse_vec, "morse.txt", letters_map_enc, letters_map_dec); // see Functions.h
 
-		temp_char = temp[0];
-		temp_code = temp.substr(1, string::npos);
-		//temp_int = get_val(temp_code);
-		Morse_Letter letter(temp_char, temp_code/*, temp_int*/);
-		morse_vec.push_back(letter);
-	}
+	/*for (vector<Morse_Letter>::iterator i = morse_vec.begin(); i != morse_vec.end(); ++i) {
+		cout << i->letter << ' ' << i->code << ' ' << letters_map_enc[i->letter] << letters_map_dec[i->code] << endl;
+	}*/
 
-	for (vector<Morse_Letter>::iterator i = morse_vec.begin(); i != morse_vec.end(); ++i) {
-		cout << i->letter << ' ' << i->code << ' ' << i->code_value << endl;
-	}
+	encode_string("abcdef\nghijkl\nmnopqrs\ntuvwxyz", letters_map_enc);
+	cout << endl;
+	decode_string("_.. ._. .. _. _._  __ ___ ._. .  ___ ..._ ._ ._.. _ .. _. .\n_... .. _ _._. ....", letters_map_dec);
+	cout << endl;
+	encode_from_file("text to encode.txt", letters_map_enc);
+	cout << endl;
+	decode_from_file("text to decode.txt", letters_map_dec);
+
 
 	//Morse_Tree x(morse_vec);
 
@@ -57,7 +62,11 @@ int main() {
 	}
 	
 
+<<<<<<< HEAD
+	//Test vector, we will need to programagically create something similar
+=======
 	// Test vector, we will need to programagically create something similar
+>>>>>>> ea741d83787f70d0189ebed67428504d70a814d5
 	vector<Morse_Letter> morse_letters = { 
 		{"a", "._"},
 		{"b", "_..."},
@@ -85,6 +94,13 @@ int main() {
 		{"x", "_.._"},
 		{"y", "_.__"},
 		{"z", "__.."}
+<<<<<<< HEAD
+	};
+	
+
+	Morse_Tree morse_tree(morse_letters);*/
+	cin.get();
+=======
 	}*/;
 	
 	//Morse_Tree morse_tree(morse_letters);
@@ -92,6 +108,7 @@ int main() {
 
 	cin.get();
 
+>>>>>>> ea741d83787f70d0189ebed67428504d70a814d5
 	return 0;
 }
 
